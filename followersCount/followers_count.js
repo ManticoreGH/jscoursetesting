7f145c6b888001521e07b2b61ee4a0f31549e9fa
ManticoreGH <sebastien.soufflet@gmail.com>
2024-12-30 21:02:02 +0100

    Adding followers page

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; // initial count to 0
+
+function increaseCount() {
+  count++; // increase count by 1
+  displayCount(); // call displayCount function
+  checkCountValue(); // call checkCountValue function
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count; // display count in the HTML element with id "countDisplay"
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
