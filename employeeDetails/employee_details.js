abc0669bbfa04a984f236f9a2e9855e9dac1c629
ManticoreGH <sebastien.soufflet@gmail.com>
2025-01-02 01:29:26 +0100

    adding typing statistics

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -8,7 +8,7 @@ const employees = [
 // Function to display all employees
 function displayEmployees() {
     const totalEmployees = employees.map((employee, index) =>
-        `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+        `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
 
